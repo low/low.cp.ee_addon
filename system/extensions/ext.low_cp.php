88dae6304c39696d9f3b2e1d0f9c1dfc6492dd60
Lodewijk Schutte <lodewijk@gmail.com>
2009-03-16 13:00:01 +0100

    v1.0.1: improved performance by using str_replace instead of preg_replace when hiding textarea helper links

--- system/extensions/ext.low_cp.php
+++ system/extensions/ext.low_cp.php
@@ -13,309 +13,311 @@
 
 if ( ! defined('EXT'))
 {
-  exit('Invalid file request');
+	exit('Invalid file request');
 }
 
 class low_cp
 {
-  var $settings       = array();
+	var $settings			= array();
 
-  var $name           = 'Low CP';
-  var $version        = '1.0';
-  var $description    = 'Control Panel according to Low';
-  var $settings_exist = 'n';
-  var $docs_url       = '';
-      
-  // -------------------------------
-  // Constructor
-  // -------------------------------
-  function low_cp($settings = '')
-  {
-    $this->settings = $settings;
-  }
-  // END low_cp
-  
-  
-  // --------------------------------
-  //  Settings
-  // --------------------------------  
-  function settings()
-  {
-    return array();
-  }
-  // END settings
-
-
-  // --------------------------------
-  //  Global CP fixes
-  // -------------------------------- 
-  function global_cp_fixes($out)
-  {
-	// output check
-	if (REQ != 'CP') return $out;
-	
-	global $IN;
-	
-	$C = $IN->GBL('C','GET');
-	$M = $IN->GBL('M','GET');
-	
-	if ( ($C == 'publish' && $M == 'entry_form') || ($C == 'edit' && $M == 'edit_entry') )
+	var $name				= 'Low CP';
+	var $version			= '1.0.1';
+	var $description		= 'Control Panel according to Low';
+	var $settings_exist		= 'n';
+	var $docs_url			= '';
+			
+	// -------------------------------
+	// Constructor
+	// -------------------------------
+	function low_cp($settings = '')
 	{
-		$out = $this->fix_link_button($out); 
-		$out = $this->fix_date_fields($out);
-		$out = $this->fix_textarea_helpers($out);
+		$this->settings = $settings;
 	}
-
-    return $out;
-  }
-  // END global_cp_fixes()
-
-  
-  // --------------------------------
-  //  No title attribute for links
-  // -------------------------------- 
-  function fix_link_button($out)
-  {
-	$find = array(
-		"var Title = prompt(title_text, theSelection);",
-		"var Link = '<a href=\"' + URL + '\" title=\"' + Title + '\">' + Name + '<'+'/a>'"
-	);
+	// END low_cp
 	
-	$replace = array(
-		"/* var Title = prompt(title_text, theSelection);",
-		"*/ var Link = '<a href=\"' + URL + '\">' + Name + '<'+'/a>'"
-	);
-
-    return str_replace($find, $replace, $out);
-  }
-  // END fix_link_button()
-
-
-  // --------------------------------
-  //  No localisation for custom date fields
-  // -------------------------------- 
-  function fix_date_fields($out)
-  {
-    return preg_replace('/<select name=\'(field_offset_\d+)\'.*?<\/select>/is','<input type="hidden" name="$1" value="n" />', $out);
-  }
-  // END fix_date_fields()
-
-
-  // --------------------------------
-  //  No spellcheck, no smilies, no glossary
-  // -------------------------------- 
-  function fix_textarea_helpers($out)
-  {
-    $out = str_replace('&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;','',$out);
-	$out = preg_replace('/<div id="smileys.*?(<div id="spellcheck_field)/is','$1',$out);
-    return preg_replace('/<a href="javascript:void\(0\);" onclick="showhide_(smileys|spellcheck|glossary)\(\'\d+\'\);return false;">/i','<a href="#" style="display:none">', $out);
-  }
-  // END fix_textarea_helpers()
-
-
-  // --------------------------------
-  //  No trackbacks in the edit list header
-  // -------------------------------- 
-  function fix_edit_list_thead($thead)
-  {
-	global $LANG;
-	return preg_replace('/(.*)<td.*'.$LANG->line('trackbacks').'.*?<\/td>/is','$1',$thead);
-  }
-  // END fix_edit_list_thead()
-
-
-  // --------------------------------
-  //  No trackbacks in the edit list body
-  // -------------------------------- 
-  function fix_edit_list_tbody($row)
-  {
-    return preg_replace('/(.*)<td.*?view_trackbacks.*?<\/td>/is', '$1', $row);
-  }
-  // END fix_edit_list_tbody()
-
-  
-  // --------------------------------
-  //  No limit to appending numbers to the url-title
-  // -------------------------------- 
-  function fix_unique_url_title()
-  {
-	global $IN, $DB, $REGX;
-
-	$entry_id  = $IN->GBL('entry_id');
-	$weblog_id = $IN->GBL('weblog_id');
-	$url_title = $IN->GBL('url_title');
-	$sql_entry = '';
-
-	if (!$url_title) return;
-
-	if ($entry_id != '')
+	
+	// --------------------------------
+	//	Settings
+	// --------------------------------	
+	function settings()
 	{
-		$sql_entry = "AND entry_id != '{$entry_id}'";
-		$url_query = $DB->query("SELECT url_title FROM exp_weblog_titles WHERE entry_id = '{$entry_id}'");
+		return array();
+	}
+	// END settings
 
-		if ($url_query->row['url_title'] != $url_title)
+
+	// --------------------------------
+	//	Global CP fixes
+	// -------------------------------- 
+	function global_cp_fixes($out)
+	{
+		// output check
+		if (REQ != 'CP') return $out;
+	
+		global $IN;
+	
+		$C = $IN->GBL('C','GET');
+		$M = $IN->GBL('M','GET');
+	
+		if ( ($C == 'publish' && $M == 'entry_form') || ($C == 'edit' && $M == 'edit_entry') )
+		{
+			$out = $this->fix_link_button($out); 
+			$out = $this->fix_date_fields($out);
+			$out = $this->fix_textarea_helpers($out);
+		}
+
+		return $out;
+	}
+	// END global_cp_fixes()
+
+	
+	// --------------------------------
+	//	No title attribute for links
+	// -------------------------------- 
+	function fix_link_button($out)
+	{
+		$find = array(
+			"var Title = prompt(title_text, theSelection);",
+			"var Link = '<a href=\"' + URL + '\" title=\"' + Title + '\">' + Name + '<'+'/a>'"
+		);
+	
+		$replace = array(
+			"/* var Title = prompt(title_text, theSelection);",
+			"*/ var Link = '<a href=\"' + URL + '\">' + Name + '<'+'/a>'"
+		);
+
+		return str_replace($find, $replace, $out);
+	}
+	// END fix_link_button()
+
+
+	// --------------------------------
+	//	No localisation for custom date fields
+	// -------------------------------- 
+	function fix_date_fields($out)
+	{
+		return preg_replace('/<select name=\'(field_offset_\d+)\'.*?<\/select>/is','<input type="hidden" name="$1" value="n" />', $out);
+	}
+	// END fix_date_fields()
+
+
+	// --------------------------------
+	//	No spellcheck, no smilies, no glossary
+	// -------------------------------- 
+	function fix_textarea_helpers($out)
+	{
+		$out = str_replace('&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;','',$out);
+		$out = preg_replace('/<div id="smileys.*?(<div id="spellcheck_field)/is','$1',$out);
+
+		// keeping things light-weight, using str_replace instead of preg_replace
+		$bin = array('spellcheck', 'glossary', 'smileys');
+		foreach ($bin AS $trash)
+		{
+			$out = str_replace('onclick="showhide_'.$trash, 'style="display:none" onclick="showhide_'.$trash, $out);
+		}
+		
+		return $out;
+	}
+	// END fix_textarea_helpers()
+
+
+	// --------------------------------
+	//	No trackbacks in the edit list header
+	// -------------------------------- 
+	function fix_edit_list_thead($thead)
+	{
+		global $LANG;
+		return preg_replace('/(.*)<td.*'.$LANG->line('trackbacks').'.*?<\/td>/is','$1',$thead);
+	}
+	// END fix_edit_list_thead()
+
+
+	// --------------------------------
+	//	No trackbacks in the edit list body
+	// -------------------------------- 
+	function fix_edit_list_tbody($row)
+	{
+		return preg_replace('/(.*)<td.*?view_trackbacks.*?<\/td>/is', '$1', $row);
+	}
+	// END fix_edit_list_tbody()
+
+	
+	// --------------------------------
+	//	No limit to appending numbers to the url-title
+	// -------------------------------- 
+	function fix_unique_url_title()
+	{
+		global $IN, $DB, $REGX;
+
+		$entry_id  = $IN->GBL('entry_id');
+		$weblog_id = $IN->GBL('weblog_id');
+		$url_title = $IN->GBL('url_title');
+		$sql_entry = '';
+
+		if (!$url_title) return;
+
+		if ($entry_id != '')
+		{
+			$sql_entry = "AND entry_id != '{$entry_id}'";
+			$url_query = $DB->query("SELECT url_title FROM exp_weblog_titles WHERE entry_id = '{$entry_id}'");
+
+			if ($url_query->row['url_title'] != $url_title)
+			{
+				$url_title = $REGX->create_url_title($url_title);
+				// and remove possible digit from the end
+				$url_title = preg_replace('/([^_\-])(\d+)?$/','$1',$url_title);
+			}
+
+		}
+		else
 		{
 			$url_title = $REGX->create_url_title($url_title);
-			// and remove possible digit from the end
-			$url_title = preg_replace('/([^_\-])(\d+)?$/','$1',$url_title);
 		}
 
-	}
-	else
-	{
-	  	$url_title = $REGX->create_url_title($url_title);
-	}
+		// Is the url_title a pure number? Let the main code handle it.
+		if (is_numeric($url_title)) return;
 
-	// Is the url_title a pure number?  Let the main code handle it.
-	if (is_numeric($url_title)) return;
-
-	// Is the URL Title empty?  Let the main code handle it.
-	if (trim($url_title) == '') return;
+		// Is the URL Title empty? Let the main code handle it.
+		if (trim($url_title) == '') return;
 	
-	/** ---------------------------------
-	/**  Is URL title unique?
-	/** ---------------------------------*/
+		/** ---------------------------------
+		/**	Is URL title unique?
+		/** ---------------------------------*/
 
-	$sql = "SELECT url_title FROM exp_weblog_titles WHERE url_title REGEXP '^{$url_title}[[:digit:]]+$' AND weblog_id = '{$weblog_id}' {$sql_entry} ORDER BY entry_id ASC";
-	$query = $DB->query($sql);
-	$titles = array();
-	foreach($query->result AS $row)
-	{
-		$titles[] = $row['url_title'];
-	}
-	
-	// nothing found? No problem!
-	if (!count($titles)) return;
-
-	$unique = FALSE;
-	$i = count($titles);
-
-	while ($unique == FALSE)
-	{
-		$temp = ($i == 0) ? $url_title : $url_title.$i;
-		$i++;
-		
-		if (!in_array($temp, $titles)) // BINGO!
+		$sql = "SELECT url_title FROM exp_weblog_titles WHERE url_title REGEXP '^{$url_title}[[:digit:]]+$' AND weblog_id = '{$weblog_id}' {$sql_entry} ORDER BY entry_id ASC";
+		$query = $DB->query($sql);
+		$titles = array();
+		foreach($query->result AS $row)
 		{
-			$_POST['url_title'] = $temp;
-			$unique = TRUE;
+			$titles[] = $row['url_title'];
+		}
+	
+		// nothing found? No problem!
+		if (!count($titles)) return;
+
+		$unique = FALSE;
+		$i = count($titles);
+
+		while ($unique == FALSE)
+		{
+			$temp = ($i == 0) ? $url_title : $url_title.$i;
+			$i++;
+		
+			if (!in_array($temp, $titles)) // BINGO!
+			{
+				$_POST['url_title'] = $temp;
+				$unique = TRUE;
+			}
 		}
 	}
-  }
-  // END fix_unique_url_title()
+	// END fix_unique_url_title()
 
 
-  
-  // --------------------------------
-  //  Activate Extension
-  // --------------------------------
-  function activate_extension()
-  {
-    global $DB, $PREFS;
-    
-    $DB->query(
-      $DB->insert_string(
-        $PREFS->ini('db_prefix').'_extensions',
-        array(
-          'extension_id' => '',
-          'class'        => __CLASS__,
-          'method'       => "global_cp_fixes",
-          'hook'         => "show_full_control_panel_end",
-          'settings'     => '',
-          'priority'     => 5,
-          'version'      => $this->version,
-          'enabled'      => "y"
-        )
-      )
-    ); // end db->query
+	
+	// --------------------------------
+	//	Activate Extension
+	// --------------------------------
+	function activate_extension()
+	{
+		global $DB, $PREFS;
+		
+		$DB->query(
+			$DB->insert_string(
+				$PREFS->ini('db_prefix').'_extensions',
+				array(
+					'extension_id'	=> '',
+					'class'			=> __CLASS__,
+					'method'		=> "global_cp_fixes",
+					'hook'			=> "show_full_control_panel_end",
+					'settings'		=> '',
+					'priority'		=> 5,
+					'version'		=> $this->version,
+					'enabled'		=> "y"
+				)
+			)
+		); // end db->query
 
 	$DB->query(
-	  $DB->insert_string(
-	    $PREFS->ini('db_prefix').'_extensions',
-	    array(
-	      'extension_id' => '',
-	      'class'        => __CLASS__,
-	      'method'       => "fix_edit_list_thead",
-	      'hook'         => "edit_entries_modify_tableheader",
-	      'settings'     => '',
-	      'priority'     => 5,
-	      'version'      => $this->version,
-	      'enabled'      => "y"
-	    )
-	  )
+		$DB->insert_string(
+			$PREFS->ini('db_prefix').'_extensions',
+			array(
+				'extension_id'	=> '',
+				'class'			=> __CLASS__,
+				'method'		=> "fix_edit_list_thead",
+				'hook'			=> "edit_entries_modify_tableheader",
+				'settings'		=> '',
+				'priority'		=> 5,
+				'version'		=> $this->version,
+				'enabled'		=> "y"
+			)
+		)
 	); // end db->query
 	
 	$DB->query(
-	  $DB->insert_string(
-	    $PREFS->ini('db_prefix').'_extensions',
-	    array(
-	      'extension_id' => '',
-	      'class'        => __CLASS__,
-	      'method'       => "fix_edit_list_tbody",
-	      'hook'         => "edit_entries_modify_tablerow",
-	      'settings'     => '',
-	      'priority'     => 5,
-	      'version'      => $this->version,
-	      'enabled'      => "y"
-	    )
-	  )
+		$DB->insert_string(
+			$PREFS->ini('db_prefix').'_extensions',
+			array(
+				'extension_id'	=> '',
+				'class'			=> __CLASS__,
+				'method'		=> "fix_edit_list_tbody",
+				'hook'			=> "edit_entries_modify_tablerow",
+				'settings'		=> '',
+				'priority'		=> 5,
+				'version'		=> $this->version,
+				'enabled'		=> "y"
+			)
+		)
 	); // end db->query
 	
 	$DB->query(
-	  $DB->insert_string(
-	    $PREFS->ini('db_prefix').'_extensions',
-	    array(
-	      'extension_id' => '',
-	      'class'        => __CLASS__,
-	      'method'       => "fix_unique_url_title",
-	      'hook'         => "submit_new_entry_start",
-	      'settings'     => '',
-	      'priority'     => 5,
-	      'version'      => $this->version,
-	      'enabled'      => "y"
-	    )
-	  )
+		$DB->insert_string(
+			$PREFS->ini('db_prefix').'_extensions',
+			array(
+				'extension_id'	=> '',
+				'class'			=> __CLASS__,
+				'method'		=> "fix_unique_url_title",
+				'hook'			=> "submit_new_entry_start",
+				'settings'		=> '',
+				'priority'		=> 5,
+				'version'		=> $this->version,
+				'enabled'		=> "y"
+			)
+		)
 	); // end db->query
 
-  }
-  // END activate_extension
-   
-   
-  // --------------------------------
-  //  Update Extension
-  // --------------------------------  
-  function update_extension($current='')
-  {
-    global $DB, $PREFS;
-    
-    if ($current == '' OR $current == $this->version)
-    {
-      return FALSE;
-    }
-    
-    if ($current <= '1.0')
-    {
-      // Update to next version: 1.1
-      // Just a setting update...
-    }
-    
-    $DB->query("UPDATE ".$PREFS->ini('db_prefix')."_extensions 
-                SET version = '".$DB->escape_str($this->version)."' 
-                WHERE class = '".__CLASS__."'");
-  }
-  // END update_extension
+	}
+	// END activate_extension
+	 
+	 
+	// --------------------------------
+	//	Update Extension
+	// --------------------------------	
+	function update_extension($current='')
+	{
+		global $DB, $PREFS;
+		
+		if ($current == '' OR $current == $this->version)
+		{
+			return FALSE;
+		}
+		
+		$DB->query("UPDATE ".$PREFS->ini('db_prefix')."_extensions 
+								SET version = '".$DB->escape_str($this->version)."' 
+								WHERE class = '".__CLASS__."'");
+	}
+	// END update_extension
 
-  // --------------------------------
-  //  Disable Extension
-  // --------------------------------
-  function disable_extension()
-  {
-    global $DB, $PREFS;
-    
-    $DB->query("DELETE FROM ".$PREFS->ini('db_prefix')."_extensions WHERE class = '".__CLASS__."'");
-  }
-  // END disable_extension
-   
+	// --------------------------------
+	//	Disable Extension
+	// --------------------------------
+	function disable_extension()
+	{
+		global $DB, $PREFS;
+		
+		$DB->query("DELETE FROM ".$PREFS->ini('db_prefix')."_extensions WHERE class = '".__CLASS__."'");
+	}
+	// END disable_extension
+	 
 }
 // END CLASS
 ?>
